1092e2993d96f41c3d7d5100674675547ed4221c
bekdev01 <bekzod.m070@gmail.com>
2022-05-27 17:29:29 +0500

    start

---
+++ src/main/java/uz/brand/natty/NattyApplication.java
@@ -0,0 +1,13 @@
+package uz.brand.natty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NattyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NattyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/brand/natty/NattyApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.brand.natty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NattyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
